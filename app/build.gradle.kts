0517137a0bf302b9ddec55d9bcc8e62cc3d41f1e
Nathan Welch <nathan.a.welch@outlook.com>
2024-04-27 02:05:18 -0500

    add retrofit to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,12 +65,16 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.retrofit2)
+
     testImplementation(libs.junit)
     testImplementation(libs.robolectric)
     testImplementation(libs.androidx.junit)
+    testImplementation(libs.androidx.ui.test.junit4)
+
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    testImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
